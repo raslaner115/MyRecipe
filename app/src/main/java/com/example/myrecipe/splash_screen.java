d35f720c620b829d16e89e0642c3ab808108f2e8
ralaner115 <rassdhhl@gmail.com>
2020-11-15 16:10:51 +0200

    release

--- app/src/main/java/com/example/myrecipe/splash_screen.java
+++ app/src/main/java/com/example/myrecipe/splash_screen.java
@@ -6,19 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-public class spalsh_screen extends AppCompatActivity {
+public class splash_screen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_spalsh_screen);
+        setContentView(R.layout.activity_splash_screen);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent=new Intent(spalsh_screen.this,Login.class);
+                Intent intent=new Intent(splash_screen.this,Login.class);
                 startActivity(intent);
-
                 finish();
             }
         },3000);
