acc8ef92b2b0c4cf26d51c01930297c95a9b3f74
ralaner115 <rassdhhl@gmail.com>
2020-11-22 21:07:54 +0200

    /

--- app/src/main/java/com/example/myrecipe/Main.java
+++ app/src/main/java/com/example/myrecipe/Main.java
@@ -1,9 +1,6 @@
 package com.example.myrecipe;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,21 +10,6 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        ImageView propic=findViewById(R.id.ProPic);
 
-        String email = (String)getIntent().getSerializableExtra("email");
-        String username = (String)getIntent().getSerializableExtra("username");
-        String name = (String)getIntent().getSerializableExtra("name");
-
-        propic.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Main.this, profile.class);
-                intent.putExtra("email", email);
-                intent.putExtra("name", username);
-                intent.putExtra("username",name);
-                startActivity(intent);
-            }
-        });
     }
 }
