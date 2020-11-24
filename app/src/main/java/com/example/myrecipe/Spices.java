1a5e901c683fa0911b6a3ccdce5b999363bba932
ralaner115 <rassdhhl@gmail.com>
2020-11-24 11:15:31 +0200

    /

--- app/src/main/java/com/example/myrecipe/Spices.java
+++ app/src/main/java/com/example/myrecipe/Spices.java
@@ -1,136 +1,103 @@
 package com.example.myrecipe;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class Spices extends AppCompatActivity {
+public class Spices extends AppCompatActivity implements View.OnClickListener {
+
+    final int[] sa = {0, 0, 0, 0, 0, 0};
 
 
+    Button Carom = findViewById(R.id.Carom);
+    Button salt = findViewById(R.id.salt);
+    Button Cinnamon = findViewById(R.id.Cinnamon);
+    Button pepper = findViewById(R.id.pepper);
+    Button Cloves = findViewById(R.id.Cloves);
+    Button Ginger = findViewById(R.id.Ginger);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spices);
 
-        final int[] sa = {0,0,0,0,0,0};
+
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRefU = database.getReference((String)getIntent().getSerializableExtra("username"));
+        DatabaseReference myRefU = database.getReference((String) getIntent().getSerializableExtra("username"));
 
-        String[] spices;
-        Button salt=findViewById(R.id.salt);
-        TextView saltA=findViewById(R.id.saltA);
 
-        salt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (sa[0]==0){
-                    myRefU.child((String)getIntent().getSerializableExtra("username")).child("spices").child("salt").setValue("true");
+        TextView CaromA = findViewById(R.id.CaromA);
+        TextView GingerA = findViewById(R.id.GingerA);
+        TextView ClovesA = findViewById(R.id.ClovesA);
+        TextView pepperA = findViewById(R.id.pepperA);
+        TextView CinnamonA = findViewById(R.id.CinnamonA);
+        TextView saltA = findViewById(R.id.saltA);
+
+         Button[] spices = {salt, pepper, Cinnamon, Cloves, Ginger, Carom};
+
+        for (int i = 0; i < spices.length; i++) {
+            spices[i].setOnClickListener(this);
+        }
+
+    }
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.salt:
+                if (sa[0]==0) {
+                    Toast.makeText(getApplicationContext(), "salt selected", Toast.LENGTH_SHORT).show();
+                    salt.setBackgroundColor(Color.GREEN);
                     sa[0]++;
                 }
-                else if (sa[0] ==1){
-                    myRefU.child((String)getIntent().getSerializableExtra("username")).child("spices").child("salt").setValue("false");
+                else {
+                    Toast.makeText(getApplicationContext(), "salt unselected", Toast.LENGTH_SHORT).show();
+                    salt.setBackgroundColor(Color.LTGRAY);
                     sa[0]--;
                 }
+            case R.id.Carom:
 
-            }
-        });
-        //__________________________________________________________________________________________
-
-        Button pepper=findViewById(R.id.pepper);
-        TextView pepperA=findViewById(R.id.pepperA);
-
-        pepper.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (sa[1]==0){
-                    myRefU.child((String)getIntent().getSerializableExtra("username")).child("spices").child("pepper").setValue("true");
-                    sa[1]++;
+                if (sa[1]==0) {
+                    Toast.makeText(getApplicationContext(), "carom selected", Toast.LENGTH_SHORT).show();
+                    salt.setBackgroundColor(Color.GREEN);
+                    sa[0]++;
                 }
-                else if (sa[1] ==1){
-                    myRefU.child((String)getIntent().getSerializableExtra("username")).child("spices").child("pepper").setValue("false");
+                else {
+                    Toast.makeText(getApplicationContext(), "carom unselected", Toast.LENGTH_SHORT).show();
+                    salt.setBackgroundColor(Color.LTGRAY);
                     sa[1]--;
                 }
-            }
-        });
-        //__________________________________________________________________________________________
-        Button Cinnamon=findViewById(R.id.Cinnamon);
-        TextView CinnamonA=findViewById(R.id.CinnamonA);
+            case R.id.Cinnamon:
 
-        Cinnamon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (sa[2]==0){
-                    myRefU.child((String)getIntent().getSerializableExtra("username")).child("spices").child("Cinnamon").setValue("true");
+                if (sa[2]==0) {
+                    Toast.makeText(getApplicationContext(), "salt selected", Toast.LENGTH_SHORT).show();
+                    salt.setBackgroundColor(Color.GREEN);
                     sa[2]++;
                 }
-                else if (sa[2] ==1){
-                    myRefU.child((String)getIntent().getSerializableExtra("username")).child("spices").child("Cinnamon").setValue("false");
+                else {
+                    Toast.makeText(getApplicationContext(), "salt unselected", Toast.LENGTH_SHORT).show();
+                    salt.setBackgroundColor(Color.LTGRAY);
                     sa[2]--;
                 }
-            }
-        });
-        //__________________________________________________________________________________________
+            case R.id.pepper:
 
-        Button Cloves=findViewById(R.id.Cloves);
-        TextView ClovesA=findViewById(R.id.ClovesA);
-
-        Cloves.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (sa[3]==0){
-                    myRefU.child((String)getIntent().getSerializableExtra("username")).child("spices").child("Cloves").setValue("true");
+                if (sa[3]==0) {
+                    Toast.makeText(getApplicationContext(), "salt selected", Toast.LENGTH_SHORT).show();
+                    salt.setBackgroundColor(Color.GREEN);
                     sa[3]++;
                 }
-                else if (sa[3] ==1){
-                    myRefU.child((String)getIntent().getSerializableExtra("username")).child("spices").child("Cloves").setValue("false");
+                else {
+                    Toast.makeText(getApplicationContext(), "salt unselected", Toast.LENGTH_SHORT).show();
+                    salt.setBackgroundColor(Color.LTGRAY);
                     sa[3]--;
                 }
-            }
-        });
-        //__________________________________________________________________________________________
-
-        Button Ginger=findViewById(R.id.Ginger);
-        TextView GingerA=findViewById(R.id.GingerA);
-
-        Ginger.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (sa[4]==0){
-                    myRefU.child((String)getIntent().getSerializableExtra("username")).child("spices").child("ginger").setValue("true");
-                    sa[4]++;
-                }
-                else if (sa[4] ==1){
-                    myRefU.child((String)getIntent().getSerializableExtra("username")).child("spices").child("ginger").setValue("false");
-                    sa[4]--;
-                }
-            }
-        });
-        //__________________________________________________________________________________________
-
-        Button Carom=findViewById(R.id.Carom);
-        TextView CaromA=findViewById(R.id.CaromA);
-
-
-        Carom.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (sa[5]==0){
-                    myRefU.child((String)getIntent().getSerializableExtra("username")).child("spices").child("Carom").setValue("true");
-                    sa[5]++;
-                }
-                else if (sa[5] ==1){
-                    myRefU.child((String)getIntent().getSerializableExtra("username")).child("spices").child("Carom").setValue("false");
-                    sa[5]--;
-                }
-            }
-        });
-        //__________________________________________________________________________________________
-
+        }
     }
 }
