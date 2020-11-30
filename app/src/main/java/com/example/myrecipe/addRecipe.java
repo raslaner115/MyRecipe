1a3841ebce404de9409a2214760e1f0ef38710bd
moaad <rakaz.hammad12@gmail.com>
2020-11-30 12:49:23 +0200

    gfchjtfgrhrtedhtfh

--- app/src/main/java/com/example/myrecipe/addRecipe.java
+++ app/src/main/java/com/example/myrecipe/addRecipe.java
@@ -9,6 +9,15 @@ import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Map;
+
 public class addRecipe<adapter> extends AppCompatActivity {
 
     String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry",
@@ -20,6 +29,27 @@ public class addRecipe<adapter> extends AppCompatActivity {
 
         ImageView addB=findViewById(R.id.addB);
 
+        final int[] i = {0,100};
+        final String[] childs=new String[i[1]];
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child((String)getIntent().getSerializableExtra("username"));
+        ref.addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        //Get map of users in datasnapshot
+
+
+                        for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                            childs[i[0]]=postSnapshot.getValue(addRecipe.class).toString();
+                            i[0]++;
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        //handle databaseError
+                    }
+                });
 
         addB.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,13 +59,18 @@ public class addRecipe<adapter> extends AppCompatActivity {
                 intentt.putExtra("email",(String)getIntent().getSerializableExtra("email"));
                 intentt.putExtra("username",(String)getIntent().getSerializableExtra("username"));
                 startActivity(intentt);
+
+
             }
         });
-        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, mobileArray);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview,childs);
         ListView listView = (ListView) findViewById(R.id.mobile_list);
         listView.setAdapter(adapter);
-    }
 
+
+
+    }
 
 
     public void onBackPressed() {
