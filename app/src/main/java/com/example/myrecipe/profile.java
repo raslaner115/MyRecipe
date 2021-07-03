efade8782e368396efdf19a18dcb94b607389931
ralaner115 <rassdhhl@gmail.com>
2021-07-03 21:23:26 +0300

    +

--- app/src/main/java/com/example/myrecipe/profile.java
+++ app/src/main/java/com/example/myrecipe/profile.java
@@ -1,9 +1,14 @@
 package com.example.myrecipe;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class profile extends AppCompatActivity {
 
@@ -12,18 +17,19 @@ public class profile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        FirebaseStorage  mFirebaseStorage = FirebaseStorage.getInstance().getReference().getStorage();
+        StorageReference desertRef = FirebaseStorage.getInstance().getReference().child((String)getIntent().getSerializableExtra("username")).child("my recipe").child((String)getIntent().getSerializableExtra("recipename"));
+        StorageReference photoRef = mFirebaseStorage.getReferenceFromUrl(String.valueOf(desertRef));
+
         TextView email2=findViewById(R.id.email2);
         TextView username2=findViewById(R.id.username2);
-        TextView name2=findViewById(R.id.name2);
+        TextView Fname2=findViewById(R.id.Fname2);
+        ImageView ProPic=findViewById(R.id.ProPic);
 
-        String email = (String)getIntent().getSerializableExtra("email");
-        String username = (String)getIntent().getSerializableExtra("username");
-        String name = (String)getIntent().getSerializableExtra("name");
-
-        email2.setText(email);
-        username2.setText(username);
-        name2.setText(name);
-
+        email2.setText((String)getIntent().getSerializableExtra("email"));
+        username2.setText((String)getIntent().getSerializableExtra("username"));
+        Fname2.setText((String)getIntent().getSerializableExtra("name"));
+        ProPic.setImageURI(Uri.parse(photoRef.toString()));
 
     }
 }
